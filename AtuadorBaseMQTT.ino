a585e53cb5b58b619710760ccf54795f27f99c04
lennedy <lennedy@gmail.com>
2024-08-20 10:47:30 -0300

    Alteração de biblioteca NFC
    Alteração da biblioteca NfcAdapter para PN532 com o objetivo de identificar problemas

--- AtuadorBaseMQTT.ino
+++ AtuadorBaseMQTT.ino
@@ -8,18 +8,19 @@
 #include <Wire.h>
 #include <PN532_I2C.h>
 #include <PN532.h>
-#include <NfcAdapter.h>
+//#include <NfcAdapter.h>
 
-#define DATA_INTERVAL 10*60000       // Intervalo para adquirir novos dados do sensor (milisegundos).
+#define DATA_INTERVAL 20000       // Intervalo para adquirir novos dados do sensor (milisegundos).
 // Os dados serão publidados depois de serem adquiridos valores equivalentes a janela do filtro
-#define AVAILABLE_INTERVAL 2*60000  // Intervalo para enviar sinais de available (milisegundos)
+#define AVAILABLE_INTERVAL 60000  // Intervalo para enviar sinais de available (milisegundos)
 #define READ_SENSOR_INTERVAL 5000  // Intervalo para fazer leituras consecutivas da chave (milisegundos)
 #define LED_INTERVAL_MQTT 1000        // Intervalo para piscar o LED quando conectado no broker
 #define JANELA_FILTRO 1         // Número de amostras do filtro para realizar a média
 
 //declaracao de objetos para uso na 
 PN532_I2C pn532_i2c(Wire);
-PN532 nfc(pn532_i2c);
+PN532 nfc = PN532(pn532_i2c);
+//PN532 nfc(pn532_i2c);
 //NfcAdapter nfc = NfcAdapter(pn532_i2c);
 
 byte RESET_PIN = 14;
@@ -34,33 +35,47 @@ unsigned long sensorReadTimePrev = 0;        // irá amazenar a última vez que 
 
 String idChave;
 HomeAssistant HA;  //trata da configuuração dos dispositivos no home assistant
+bool pn532Fail=true;
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("what the hell");
 
   pinMode(BLINK_PIN, OUTPUT);
   pinMode(ACIONAMENTO_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(BUZZER_PIN, OUTPUT); // Sets the echoPin as an Input  
-  pinMode(RESET_PIN, OUTPUT);
+//  pinMode(BUZZER_PIN, OUTPUT); // Sets the echoPin as an Input  
+//  pinMode(RESET_PIN, OUTPUT);
 
-  digitalWrite(RESET_PIN, LOW);
-  delay(1000);
-  digitalWrite(RESET_PIN, HIGH);
-  delay(1000);
-  
+//  digitalWrite(RESET_PIN, LOW);
+//  delay(1000);
+//  digitalWrite(RESET_PIN, HIGH);
+//  delay(1000);
+
+  Serial.println("Antes do begin");
+
   nfc.begin(); //inicia o leitor de nfc/rfid
 
-  nfc.SAMConfig();
-  
+  Serial.println("Depois do begin");
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.println("Didn't find PN53x board");
+    pn532Fail=true;
+  }
+  else{
+    nfc.SAMConfig();
+    pn532Fail=false;
+    Serial.println(versiondata);
+  }
 
   // Optional functionalities of EspMQTTClient
   //client.enableMQTTPersistence();
-  //client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
   //client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
   client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
   client.enableLastWillMessage(TOPIC_AVAILABLE, "offline");  // You can activate the retain flag by setting the third parameter to true
-  client.setKeepAlive(60); 
+  //client.setKeepAlive(60); 
   WiFi.mode(WIFI_STA);
 }
 
@@ -108,9 +123,36 @@ bool readSensor() {
     return true;
   }*/
 
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    //Serial.println("Didn't find PN53x board");
+    pn532Fail=true;
+  }
 
-  return false;
+  if(pn532Fail){
+    //Serial.println("Erro");
+    return false;
+  }
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 5);
+
+  if(success){
+
+    idChave = "";
+    
+    for(int i = 0; i < uidLength; i++) {
+      if(uid[i] < 0x10) {
+        idChave += '0';
+      }
+
+      idChave += String(uid[i], HEX);
+
+      if(i < uidLength-1)
+        idChave += ' ';
+    }
+  }
+
+  return success;
 }
 
 void metodoPublisher() {
@@ -120,10 +162,10 @@ void metodoPublisher() {
   jsonDoc["RSSI"] = WiFi.RSSI();
   jsonDoc["state"] = "LOCKED";
 
-//  if( nfc.isInitiated())
-//    jsonDoc["erro"] = false;
-//  else
-//    jsonDoc["erro"] = true;
+  if(pn532Fail)
+    jsonDoc["erro PN532"] = true;
+  else
+    jsonDoc["erro PN532"] = false;
 
   jsonDoc["heap"]     = ESP.getFreeHeap();
   jsonDoc["stack"]    = ESP.getFreeContStack();
